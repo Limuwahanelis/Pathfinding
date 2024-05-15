6d8480b4c0d77843b66e240db9f8381168de6934
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-05-15 16:10:30 +0200

    Add project

---
+++ Pathfinding/Assets/Scripts/TileMapSetter.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class TileMapSetter : MonoBehaviour
+{
+    [SerializeField] Material _mat;
+    [SerializeField] GameObject _mapQuad;
+    [SerializeField] Vector2 _gridSize;
+    Vector2 _oldGridSize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _mat.SetVector("_Vector2", _gridSize);
+        Vector3 halfSizeIncrease= new Vector3(-math.abs(_mapQuad.transform.localScale.x - _gridSize.x) / 2, 0, math.abs(_mapQuad.transform.localScale.y - _gridSize.y) / 2);
+        _mapQuad.transform.localScale = _gridSize;
+        _mapQuad.transform.position += halfSizeIncrease;
+        _mapQuad.transform.position += new Vector3(-0.5f, 0, 0);
+         _mapQuad.transform.position += new Vector3(0, 0, -0.5f);
+        _oldGridSize = _gridSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            if (_gridSize == _oldGridSize) return;
+            _mat.SetVector("_Vector2", _gridSize);
+            Vector3 halfSizeIncrease = new Vector3(-math.abs(_mapQuad.transform.localScale.x - _gridSize.x) / 2, 0, math.abs(_mapQuad.transform.localScale.y - _gridSize.y) / 2);
+            halfSizeIncrease = new Vector3((_mapQuad.transform.localScale.x - _gridSize.x > 0 ? -1 : 1) * halfSizeIncrease.x, 0, (_mapQuad.transform.localScale.y - _gridSize.y > 0 ? -1 : 1) * halfSizeIncrease.z);
+            _mapQuad.transform.localScale = _gridSize;
+            _mapQuad.transform.position += halfSizeIncrease;
+            _oldGridSize = _gridSize;
+        }
+    }
+
+    public void UpdatGridVertically(float newSize)
+    {
+
+        _gridSize.x = (int)newSize;
+        if (_gridSize == _oldGridSize) return;
+        _mat.SetVector("_Vector2", _gridSize);
+        Vector3 halfSizeIncrease = new Vector3(-math.abs(_mapQuad.transform.localScale.x - _gridSize.x) / 2, 0, math.abs(_mapQuad.transform.localScale.y - _gridSize.y) / 2);
+        halfSizeIncrease = new Vector3((_mapQuad.transform.localScale.x - _gridSize.x > 0 ? -1 : 1) * halfSizeIncrease.x, 0, (_mapQuad.transform.localScale.y - _gridSize.y > 0 ? -1 : 1) * halfSizeIncrease.z);
+        _mapQuad.transform.localScale = _gridSize;
+        _mapQuad.transform.position += halfSizeIncrease;
+        _oldGridSize = _gridSize;
+    }
+
+    public void UpdatGridHorizontally(float newSize)
+    {
+        _gridSize.y = (int)newSize;
+        if (_gridSize == _oldGridSize) return;
+        _mat.SetVector("_Vector2", _gridSize);
+        Vector3 halfSizeIncrease = new Vector3(-math.abs(_mapQuad.transform.localScale.x - _gridSize.x) / 2, 0, math.abs(_mapQuad.transform.localScale.y - _gridSize.y) / 2);
+        halfSizeIncrease = new Vector3((_mapQuad.transform.localScale.x - _gridSize.x > 0 ? -1 : 1) * halfSizeIncrease.x, 0, (_mapQuad.transform.localScale.y - _gridSize.y > 0 ? -1 : 1) * halfSizeIncrease.z);
+        _mapQuad.transform.localScale = _gridSize;
+        _mapQuad.transform.position += halfSizeIncrease;
+        _oldGridSize = _gridSize;
+    }
+}
---
+++ Pathfinding/Assets/Scripts/UI/SliderWithTextInput.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.UI;
+using static UnityEngine.Rendering.DebugUI;
+
+public class SliderWithTextInput : MonoBehaviour
+{
+    [SerializeField] TMP_InputField _inputTextField;
+    [SerializeField] Slider _slider;
+    [SerializeField] float _minValue;
+    [SerializeField] float _maxValue;
+    [SerializeField] float _defaultValue;
+    [SerializeField] bool _wholeNumbers;
+    [SerializeField] bool _defaultDecimal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _slider.minValue = _minValue;
+        _slider.maxValue = _maxValue;
+    }
+    public void UpdateSlider(string text)
+    {
+        float value;
+        if (string.IsNullOrEmpty(text)) value = _minValue;
+        else value = float.Parse(text);
+        _slider.SetValueWithoutNotify(value);
+    }
+    public void UpdateText(float value)
+    {
+        float numberToShow = value;
+        if (_defaultDecimal) _inputTextField.SetTextWithoutNotify(numberToShow.ToString());
+        else _inputTextField.text = numberToShow.ToString("0.00");
+
+    }
+
+    public void CheckText(string text)
+    {
+
+        float value;
+        string toOutput;
+        if (string.IsNullOrEmpty(text)) return;
+        else
+        {
+            if (text == ",")
+            {
+                toOutput = "0,0";
+                value = float.Parse(toOutput);
+                _inputTextField.text = toOutput;
+                _slider.value = value;
+                return;
+            }
+            value = float.Parse(text);
+            value = Mathf.Clamp(value, _minValue, _maxValue) * 1.0f;
+            if (text[text.Length - 1] == ',') toOutput = text;
+            else if (text[0] == ',') toOutput = text;
+            else toOutput = value.ToString();
+
+            value = float.Parse(text);
+            value = Mathf.Clamp(value, _minValue, _maxValue) * 1.0f;
+            _inputTextField.text = toOutput;
+            _slider.SetValueWithoutNotify(value);
+        }
+
+
+    }
+    public void CheckTextInt(string text)
+    {
+
+        int value;
+        string toOutput;
+        if (string.IsNullOrEmpty(text))
+        {
+            value = (int)_minValue;
+            toOutput = value.ToString();
+            _inputTextField.text = toOutput;
+        }
+        else
+        {
+            value = int.Parse(text);
+            value = math.clamp(value, ((int)_minValue), ((int)_maxValue));
+            toOutput = value.ToString();
+            _inputTextField.text = toOutput;
+            _slider.value = value;
+            //_slider.SetValueWithoutNotify(value);
+        }
+
+
+    }
+
+    private void OnValidate()
+    {
+        if (_wholeNumbers)
+        {
+            _minValue = Mathf.RoundToInt(_minValue);
+            _maxValue = Mathf.RoundToInt(_maxValue);
+        }
+        _slider.wholeNumbers = _wholeNumbers;
+
+         _defaultValue = math.clamp(_defaultValue, _minValue, _maxValue); 
+        if(_wholeNumbers) _defaultValue =Mathf.Round(_defaultValue);
+        UpdateText(_defaultValue);
+    }
+}
