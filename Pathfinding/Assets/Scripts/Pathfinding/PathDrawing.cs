2cddbcae27d6e90a00bf9bf1784c5eb524a9dd03
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-05-17 16:33:17 +0200

    Make PathDrawing hide path if one of its points is outside the map

--- Pathfinding/Assets/Scripts/Pathfinding/PathDrawing.cs
+++ Pathfinding/Assets/Scripts/Pathfinding/PathDrawing.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PathDrawing : MonoBehaviour
 {
+    
     [SerializeField] LineRenderer _lineRenderer;
     [SerializeField] AStarPathfinding _AStarPathfinding;
+    [SerializeField] TileMapSetter _map;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _AStarPathfinding.OnPathCreated.AddListener(DrawLine);
+        _lineRenderer.positionCount = 0;
     }
 
     // Update is called once per frame
@@ -18,15 +22,29 @@ public class PathDrawing : MonoBehaviour
         
     }
 
-    public void DrawLine()
+    public void DrawLine(List<Vector2Int> nodes)
     {
-        List<Vector3> positions = new List<Vector3>();
-        List<Vector2Int> nodes = _AStarPathfinding.GetPathPoints();
         _lineRenderer.positionCount=nodes.Count;
         for (int i = 0 ;i< nodes.Count;i++)
         {
             
             _lineRenderer.SetPosition(i, new Vector3(nodes[i].x, 0.05f, nodes[i].y));
         }
+    }
+    public void CheckIfPathIsOnMap()
+    {
+        Vector3[] positions = new Vector3[_lineRenderer.positionCount];
+        _lineRenderer.GetPositions(positions);
+        foreach (Vector3 pos in positions)
+        {
+            if(pos.x>_map.GridSize.x-1 || pos.z>_map.GridSize.y-1)
+            {
+                _lineRenderer.positionCount = 0; break;
+            }
+        }
+    }
+    private void OnDestroy()
+    {
+        _AStarPathfinding.OnPathCreated.RemoveListener(DrawLine);
     }
 }
