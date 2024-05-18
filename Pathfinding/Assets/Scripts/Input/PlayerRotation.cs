22715e07f31c02afa192c1b722a786ee74cf0201
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-05-18 15:14:33 +0200

    Refactor playerrotation

--- Pathfinding/Assets/Scripts/Input/PlayerRotation.cs
+++ Pathfinding/Assets/Scripts/Input/PlayerRotation.cs
@@ -6,15 +6,12 @@ using UnityEngine.InputSystem;
 public class PlayerRotation : MonoBehaviour
 {
     [SerializeField] float mouseRotationSpeed = 0.2f;
-    [SerializeField] float keyboardRotationSpeed = 2f;
 
-    [SerializeField] GameObject focalPoint;
+    private float screenX = Screen.width;
+    private float screenY = Screen.height;
 
-    float screenX = Screen.width;
-    float screenY = Screen.height;
-
-    float yaw = 0.0f;
-    float row = 0.0f;
+    private float yaw = 0.0f;
+    private float row = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,24 +19,11 @@ public class PlayerRotation : MonoBehaviour
         row = transform.rotation.eulerAngles.x;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        float mouseX = Mouse.current.position.x.value;
-        float mouseY = Mouse.current.position.y.value;
-        if (mouseX < 0 || mouseX > screenX || mouseY < 0 || mouseY > screenY)
-            return;
-    }
     public void RotateMouse(Vector2 delta)
     {
         yaw += delta.x * mouseRotationSpeed;
         row -= delta.y * mouseRotationSpeed;
         Rotate();
-    }
-    public void ResetPivot()
-    {
-        //focalPoint.transform.rotation = Quaternion.identity;
-        //yaw = 0;
     }
     private void Rotate()
     {
