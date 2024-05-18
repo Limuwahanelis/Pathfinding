ed25c5716c1b0b2968381cd7070efdde6006bc7c
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-05-18 18:44:25 +0200

    Update PathDrawing.cs

--- Pathfinding/Assets/Scripts/Pathfinding/PathDrawing.cs
+++ Pathfinding/Assets/Scripts/Pathfinding/PathDrawing.cs
@@ -7,12 +7,9 @@ public class PathDrawing : MonoBehaviour
 {
     
     [SerializeField] LineRenderer _lineRenderer;
-    [SerializeField] AStarPathfinding _AStarPathfinding;
-    [SerializeField] TileMapSetter _map;
     // Start is called before the first frame update
     void Start()
     {
-        _AStarPathfinding.OnPathCreated.AddListener(DrawLine);
         _lineRenderer.positionCount = 0;
     }
 
@@ -27,24 +24,11 @@ public class PathDrawing : MonoBehaviour
         _lineRenderer.positionCount=nodes.Count;
         for (int i = 0 ;i< nodes.Count;i++)
         {
-            
             _lineRenderer.SetPosition(i, new Vector3(nodes[i].x, 0.05f, nodes[i].y));
         }
     }
-    public void CheckIfPathIsOnMap()
+    public void HidePath()
     {
-        Vector3[] positions = new Vector3[_lineRenderer.positionCount];
-        _lineRenderer.GetPositions(positions);
-        foreach (Vector3 pos in positions)
-        {
-            if(pos.x>_map.GridSize.x-1 || pos.z>_map.GridSize.y-1)
-            {
-                _lineRenderer.positionCount = 0; break;
-            }
-        }
-    }
-    private void OnDestroy()
-    {
-        _AStarPathfinding.OnPathCreated.RemoveListener(DrawLine);
+        _lineRenderer.positionCount = 0;
     }
 }
